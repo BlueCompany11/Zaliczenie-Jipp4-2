132629b4fbcbd015661e905ad4dc5c5bc0b46696
BlueCompany <gronczewskim@gmail.com>
2018-05-28 00:47:14 +0200

    asd

--- Zaliczenie Jipp4 2/Form1.Designer.cs
+++ Zaliczenie Jipp4 2/Form1.Designer.cs
@@ -28,13 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonOkrag = new System.Windows.Forms.Button();
+            this.buttonProstokat = new System.Windows.Forms.Button();
+            this.buttonProsta = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBoxOkrag = new System.Windows.Forms.GroupBox();
+            this.groupBoxProstokat = new System.Windows.Forms.GroupBox();
+            this.groupBoxProsta = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonOkrag, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonProstokat, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonProsta, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 350);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonOkrag
+            // 
+            this.buttonOkrag.AutoSize = true;
+            this.buttonOkrag.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonOkrag.Location = new System.Drawing.Point(3, 3);
+            this.buttonOkrag.Name = "buttonOkrag";
+            this.buttonOkrag.Size = new System.Drawing.Size(260, 94);
+            this.buttonOkrag.TabIndex = 0;
+            this.buttonOkrag.Text = "Rysuj okrąg";
+            this.buttonOkrag.UseVisualStyleBackColor = true;
+            this.buttonOkrag.Click += new System.EventHandler(this.buttonOkrag_Click);
+            // 
+            // buttonProstokat
+            // 
+            this.buttonProstokat.AutoSize = true;
+            this.buttonProstokat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonProstokat.Location = new System.Drawing.Point(269, 3);
+            this.buttonProstokat.Name = "buttonProstokat";
+            this.buttonProstokat.Size = new System.Drawing.Size(260, 94);
+            this.buttonProstokat.TabIndex = 1;
+            this.buttonProstokat.Text = "Rysuj prostokąt";
+            this.buttonProstokat.UseVisualStyleBackColor = true;
+            this.buttonProstokat.Click += new System.EventHandler(this.buttonProstokat_Click);
+            // 
+            // buttonProsta
+            // 
+            this.buttonProsta.AutoSize = true;
+            this.buttonProsta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonProsta.Location = new System.Drawing.Point(535, 3);
+            this.buttonProsta.Name = "buttonProsta";
+            this.buttonProsta.Size = new System.Drawing.Size(262, 94);
+            this.buttonProsta.TabIndex = 2;
+            this.buttonProsta.Text = "Rysuj prostą";
+            this.buttonProsta.UseVisualStyleBackColor = true;
+            this.buttonProsta.Click += new System.EventHandler(this.buttonProsta_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.groupBoxOkrag, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.groupBoxProstokat, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.groupBoxProsta, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(800, 350);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // groupBoxOkrag
+            // 
+            this.groupBoxOkrag.AutoSize = true;
+            this.groupBoxOkrag.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOkrag.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxOkrag.Name = "groupBoxOkrag";
+            this.groupBoxOkrag.Size = new System.Drawing.Size(260, 344);
+            this.groupBoxOkrag.TabIndex = 0;
+            this.groupBoxOkrag.TabStop = false;
+            this.groupBoxOkrag.Text = "Miejsce na okrąg";
+            this.groupBoxOkrag.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBoxOkrag_Paint);
+            // 
+            // groupBoxProstokat
+            // 
+            this.groupBoxProstokat.AutoSize = true;
+            this.groupBoxProstokat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxProstokat.Location = new System.Drawing.Point(269, 3);
+            this.groupBoxProstokat.Name = "groupBoxProstokat";
+            this.groupBoxProstokat.Size = new System.Drawing.Size(260, 344);
+            this.groupBoxProstokat.TabIndex = 1;
+            this.groupBoxProstokat.TabStop = false;
+            this.groupBoxProstokat.Text = "Miejsce na prostokąt";
+            this.groupBoxProstokat.Paint += new System.Windows.Forms.PaintEventHandler(this.groupBoxProstokat_Paint);
+            // 
+            // groupBoxProsta
+            // 
+            this.groupBoxProsta.AutoSize = true;
+            this.groupBoxProsta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxProsta.Location = new System.Drawing.Point(535, 3);
+            this.groupBoxProsta.Name = "groupBoxProsta";
+            this.groupBoxProsta.Size = new System.Drawing.Size(262, 344);
+            this.groupBoxProsta.TabIndex = 2;
+            this.groupBoxProsta.TabStop = false;
+            this.groupBoxProsta.Text = "Miejsce na prostą";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Maszyny";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonOkrag;
+        private System.Windows.Forms.Button buttonProstokat;
+        private System.Windows.Forms.Button buttonProsta;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.GroupBox groupBoxOkrag;
+        private System.Windows.Forms.GroupBox groupBoxProstokat;
+        private System.Windows.Forms.GroupBox groupBoxProsta;
     }
 }
 
--- Zaliczenie Jipp4 2/Form1.cs
+++ Zaliczenie Jipp4 2/Form1.cs
@@ -12,9 +12,48 @@ namespace Zaliczenie_Jipp4_2
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() //aplikacja SDI single document interface - wyklad 4
         {
             InitializeComponent();
+
+        }
+
+        private void groupBoxOkrag_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupBoxProstokat_Paint(object sender, PaintEventArgs e)
+        {
+            int centerX = (groupBoxProstokat.Left + groupBoxProstokat.Right) / 2;
+            int centerY = (groupBoxProstokat.Top + groupBoxProstokat.Height) / 2;
+            int radius = groupBoxProstokat.Width / 2;
+
+            e.Graphics.DrawEllipse(Pens.Red, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+        }
+
+        private void buttonOkrag_Click(object sender, EventArgs e)
+        {
+            Graphics g = groupBoxOkrag.CreateGraphics();
+            int centerX = 40; //(groupBoxProstokat.Left + groupBoxProstokat.Right) / 2;
+            int centerY = 40; //(groupBoxProstokat.Top + groupBoxProstokat.Height) / 2;
+            int radius = groupBoxOkrag.Width / 32;
+            g.DrawEllipse(Pens.Red, centerX - radius, centerY - radius,
+                      radius + radius, radius + radius);
+
+        }
+
+        private void buttonProstokat_Click(object sender, EventArgs e)
+        {
+            Graphics g = groupBoxProstokat.CreateGraphics();
+            g.DrawRectangle(Pens.SeaGreen, 10, 10, 50, 50);
+        }
+
+        private void buttonProsta_Click(object sender, EventArgs e)
+        {
+            Graphics g = groupBoxProsta.CreateGraphics();
+            g.DrawLines(Pens.SeaGreen, new Point[] { new Point(10, 10), new Point(20, 20) });
         }
     }
 }
